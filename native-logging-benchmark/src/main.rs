1e42c1bff612525477ac0d7b47128759c3ba9889
Lalit <labhas@microsoft.com>
2023-04-24 14:55:29 -0700

    performance tests native logger

--- native-logging-benchmark/src/main.rs
+++ native-logging-benchmark/src/main.rs
@@ -1,3 +1,70 @@
+#[macro_use]
+extern crate log;
+use std::{error, fmt, result, sync::Arc};
+use microbench::{self, Options};
+use log::{Level, Metadata, Record};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+/* Results -
+    No logger attached - log_enabled(level) (5.0s) ...           0.423 ns/iter (0.999 R²)
+    No logger attached - log_enabled(target, level) (5.0s) ...           0.420 ns/iter (0.999 R²)
+    No logger attached - debug(text (5.0s) ...           0.219 ns/iter (0.997 R²)
+    Noop logger attached - log_enabled() (5.0s) ...           1.556 ns/iter (0.998 R²)
+    Noop logger attached - log_enabled(target, level) (5.0s) ...           1.321 ns/iter (0.998 R²)
+    Noop logger attached - debug(text (5.0s) ...           3.160 ns/iter (0.998 R²)
+ */
+
+struct NoopLogger {
+    level: Level,
+}
+
+impl log::Log for NoopLogger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= self.level
+    }
+    fn log(&self, record: &Record) {
+    }
+
+    fn flush(&self) {
+        
+    }
+}
+
+impl NoopLogger{
+    fn init(level: Level){
+        log::set_max_level(level.to_level_filter());
+        let logger = NoopLogger{level};
+        log::set_boxed_logger(Box::new(logger));
+    }
+}
+
+fn main () {
+    let options: Options = Options::default();
+
+    microbench::bench(&options, "No logger attached - log_enabled(level)", || log_enabled!(Level::Debug));
+    microbench::bench(&options, " No logger attached - log_enabled(target, level)", || log_enabled!(target:"Global", Level::Debug));
+
+    microbench::bench(&options, " No logger attached - debug(text", || debug!("expensive debug data: "));
+
+    NoopLogger::init(Level::Debug);
+    microbench::bench(&options, "Noop logger attached - log_enabled()", || log_enabled!(Level::Debug));
+    microbench::bench(&options, "Noop logger attached - log_enabled(target, level)", || log_enabled!(target:"Global", Level::Debug));
+
+    microbench::bench(&options, "Noop logger attached - debug(text", || debug!("expensive debug data: "));
+
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
 use log::Level::Debug;
 use log::{debug, log_enabled};
 use microbench::{self, Options};
@@ -24,3 +91,4 @@ fn main() {
     debug!(target: "Global", "expensive debug data: {} {}", data.x, data.y);
     }
 }
+*/
