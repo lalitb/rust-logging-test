09fabe3bd7bc8a7243f4c03cdd447a095528e254
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2023-04-24 09:47:22 -0700

    native logging performance

---
+++ native-logging-benchmark/src/main.rs
@@ -0,0 +1,24 @@
+use log::Level::Debug;
+use log::{debug, log_enabled};
+use microbench::{self, Options};
+
+struct Data{
+    x: i32,
+    y: i32
+}
+
+fn expensive_call() -> Data {
+    Data {x:10, y:100}
+}
+
+fn main() {
+    let options: Options = Options::default();
+    if log_enabled!(Debug) {
+        let data = expensive_call();
+        debug!("expensive debug data: {} {}", data.x, data.y);
+    }
+    if log_enabled!(target: "Global", Debug) {
+        let data = expensive_call();
+    debug!(target: "Global", "expensive debug data: {} {}", data.x, data.y);
+    }
+}
