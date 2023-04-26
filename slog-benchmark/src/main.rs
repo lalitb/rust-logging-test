ee8aed84f3ba21a0d51a1d30af02e43cc09f02ec
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2023-04-26 15:34:28 -0700

    fix

--- slog-benchmark/src/main.rs
+++ slog-benchmark/src/main.rs
@@ -1,9 +1,19 @@
 #[macro_use]
 extern crate slog;
 extern crate chrono;
+extern crate slog_term;
+extern crate slog_async;
+
 use microbench::{self, Options};
 
-/* slog - noop drain  (5.0s) ...              2.452 ns/iter (0.990 R²)
+use crate::slog::Drain;
+use std::fs::File;
+
+
+
+/* 
+slog - is_enabled(level) (5.0s) ...           1.218 ns/iter (0.998 R²)
+slog - noop drain  (5.0s) ...              2.412 ns/iter (0.996 R²)
 */
 
 fn main() {
@@ -11,7 +21,30 @@ fn main() {
 
     let drain = slog::Discard;
 
-    let root_logger = slog::Logger::root(drain, o!());
+    // update filter level at bootstrap
+    drain.filter_level(slog::Level::Debug).fuse();
 
-    microbench::bench(&options, "slog - noop drain ", || info!(root_logger, "Application started"));
+    let root_logger: slog::Logger = slog::Logger::root(drain, o!());
+
+   // assert!(root_logger.is_debug_enabled() == true);
+   println!(" Logger level: {}", root_logger.is_enabled(slog::Level::Error));
+
+    //runtime filter level
+    drain.filter_level(slog::Level::Error).fuse();
+    microbench::bench(&options, "slog noop - is_enabled(level)", || root_logger.is_enabled(slog::Level::Debug));
+    microbench::bench(&options, "slog noop info ", || info!(root_logger, "Application started"));
+
+    // use /dev/null with filtering
+    let mut f = File::open("/dev/null").expect("error opening");
+
+    let decorator = slog_term::PlainSyncDecorator::new(f);
+    let drain = slog_term::FullFormat::new(decorator).build().filter_level(slog::Level::Info).fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    let root_logger: slog::Logger = slog::Logger::root(drain, o!());
+    assert!(root_logger.is_enabled(slog::Level::Debug) == true);
+    microbench::bench(&options, "slog /dev/null ", || root_logger.is_enabled(slog::Level::Debug));
+    microbench::bench(&options, "slog /dev/null - is_enabled(level)", || root_logger.is_enabled(slog::Level::Debug));
+    microbench::bench(&options, "slog /dev/null - is_enabled(level)", || root_logger.is_enabled(slog::Level::Debug));
+
 }
