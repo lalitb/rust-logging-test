96727fd9c94a7e063a9b575ae70e40f784d12e7e
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2023-04-27 16:32:33 -0700

    change

--- tokio-tracing-example/src/main.rs
+++ tokio-tracing-example/src/main.rs
@@ -10,31 +10,28 @@ struct User {
 }
 
 fn main() {
-    /*   let filter = EnvFilter::from_default_env()
-    .add_directive(Level::ERROR.into()); */
-
     let filter = EnvFilter::from_default_env()
-        .add_directive("category1=error".parse().unwrap())
+        .add_directive("target1=error".parse().unwrap())
         .add_directive(Level::INFO.into());
 
     let layer = CustomLayer {}.with_filter(filter);
     tracing_subscriber::registry().with(layer).init();
 
-    info!("test123"); //default category as module path/name
+    info!("test123"); //default target as module path/name
 
-    info!(target: "category1", "something has happened");
+    info!(target: "target1", "something has happened");
 
-    event!(target: "category1", Level::ERROR, "something has happened!");
+    event!(target: "target1", Level::ERROR, "something has happened!");
 
     // structured field using struct
     let user = User {
         name: "ferris".into(),
         email: "ferris@rust-lang.org".into(),
     };
-    info!(target: "category2", event_id = 2, event_name = "event_name", user.name, user.email);
+    info!(target: "target2", event_id = 2, event_name = "event_name", user.name, user.email);
 
     //stuct field using key-value part of arguments
-    info!(target: "category2", event_id = 2, event_name = "event_name", user.name, user.email);
+    info!(target: "target2", event_id = 2, event_name = "event_name", user.name, user.email);
 
     //message template using escape
     info!(
